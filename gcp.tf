f8dbc2b0441bb4f3629ae7d64b3e67ea231ff6f9
goodahn <wnaldj1589@gmail.com>
2022-11-19 22:39:04 +0900

    프로젝트 id를 variable 로 사용

--- gcp.tf
+++ gcp.tf
@@ -1,10 +1,13 @@
 provider "google" {
-  project     = "My Project"
+  project     = var.project-id
   region      = "us-west1"
   zone        = "us-central1-a"
   credentials = var.gcp-credential
 }
 
+variable "project-id" {
+  default = ""
+}
 
 variable "gcp-credential" {
   default = ""
